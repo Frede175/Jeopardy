8fc355cea676ef96059f01b23c193484292c2629
Frede175 <fsr1998@hotmail.com>
2015-01-27 12:43:15 +0100

    27/01-15

--- js/make.js
+++ js/make.js
@@ -21,18 +21,34 @@ function clickbtn() {
 	});
 
 	$('.btnQ').unbind().click(function(event) {
-		
+		//The question for that button!!!-------------------
+
 		showPopup(4);
 	});
 
+	$('#add-subject').click(function(event) {
+		nSubjects++;
+		update();
+	});
 
+	$('#remove-subject').click(function(event) {
+		nSubjects--;
+		update();
+	});
 
+	$('#add-question').click(function(event) {
+		nQuestions++;
+		update();
+	});
+
+	$('#remove-question').click(function(event) {
+		nQuestions--;
+		update();
+	});
 
 }
 
 function generatetable() {
-
-
 
 
 	browser_height = window.innerHeight-70;
@@ -43,9 +59,34 @@ function generatetable() {
 
 	$('#make-table').height(browser_height);
 	$('#make-table').width(browser_width);
+	$('#bottom-bar').width(browser_width);
+	$('#side-bar').height(browser_height);
 
-	table(browser_width, browser_height+100, nQuestions, nSubjects);
+	table(browser_width, browser_height+120, nQuestions, nSubjects);
 
 	clickbtn();
 }
+
+function update() {
+	var hTable = setHeight(browser_height+120, nQuestions);
+	var wTable = setWidth(browser_width, nSubjects);
+
+	$('.table_box').height(hTable);
+	$('.table_box').width(wTable);
+
+	$('.subject_table_box').height(hTable);
+	$('.subject_table_box').width(wTable);
+/*	for(var i = 1; i <= nSubjects; i++){
+		for (var x = 1; x <= nQuestions; x++){
+			$('#btn_' + i + "_" + x).height(hTable);
+			$('#btn_' + i + "_" + x).width(wTable);
+		}
+	} 
+
+	if(btn_id == "add-subject"){
+
+	} */
+
+}
+
 
