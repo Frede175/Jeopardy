54bc0e7bc033919ac0f1d5315c2422a131caa25b
Frede175 <fsr1998@hotmail.com>
2015-02-16 22:09:44 +0100

    ssss

--- logged_in.php
+++ logged_in.php
@@ -39,6 +39,7 @@ sec_session_start();
 					<ul>
 						<li><a href="make-loggedin">Make a game</a></li>
 						<li><a href="Jeopardy-loggedin">Play a game</a></li>
+						<li><a href="manage-files">Manage files</a>
 						<li><a href="account-settings">Account settings</a></li>
 					</ul>
 
